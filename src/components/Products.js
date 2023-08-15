d1125eb77693f1d9f13536f8738d478c2eec6f25
zaki-20 <zakigame199@gmail.com>
2023-08-15 20:00:09 +0500

    fetched product using Thunk middleware

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,20 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { add } from '../store/cartSlice';
+import { fetchProducts } from '../store/productSlice';
 
 const Products = () => {
     const dispatch = useDispatch();
-    const [products, setProducts] = useState([]);
-    // const { data: products, status } = useSelector((state) => state.product);
+    const { data: products, status } = useSelector((state) => state.product);
 
     useEffect(() => {
-        const fetchProducts = async () => {
-            const res = await fetch('https://fakestoreapi.com/products');
-            const data = await res.json();
-            console.log(data);
-            setProducts(data);
-        };
-        fetchProducts();
+
+        dispatch(fetchProducts())
+        // const fetchProducts = async () => {
+        //     const res = await fetch('https://fakestoreapi.com/products');
+        //     const data = await res.json();
+        //     console.log(data);
+        //     setProducts(data);
+        // };
+        // fetchProducts();
     }, []);
 
     const handleAdd = (product) => {
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,23 +1,33 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux';
+import { remove } from '../store/cartSlice';
+
 
 const Cart = () => {
+
+    const dispatch = useDispatch();
+    const products = useSelector((state) => state.cart);
+
+    const handleRemove = (productId) => {
+        dispatch(remove(productId));
+    };
     return (
         <div>
             <h3>Cart</h3>
             <div className="cartWrapper">
-                {/* {products.map((product) => ( */}
-                <div className="cartCard">
-                    <img src={"product.image"} alt="" />
-                    <h5>{"product.title"}</h5>
-                    <h5>{"product.price"}</h5>
-                    <button
-                        className="btn"
-                    // onClick={() => handleRemove(product.id)}
-                    >
-                        Remove
-                    </button>
-                </div>
-                {/* ))} */}
+                {products.map((product) => (
+                    <div className="cartCard">
+                        <img src={product.image} alt="" />
+                        <h5>{product.title}</h5>
+                        <h5>{product.price}</h5>
+                        <button
+                            className="btn"
+                            onClick={() => handleRemove(product.id)}
+                        >
+                            Remove
+                        </button>
+                    </div>
+                ))}
             </div>
         </div>
     )
