72336d23290bca4a10a33564ca74ade0ae6f370f
zaki-20 <zakigame199@gmail.com>
2023-08-15 15:02:00 +0500

    project setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
@@ -10,7 +12,7 @@ function App() {
                         <Route path="/" element={<Home />}></Route>
                         <Route path="/cart" element={<Cart />}></Route>
                     </Routes>
-                </BrowserRouter>
+        </BrowserRouter>
     </div>
   );
 }
